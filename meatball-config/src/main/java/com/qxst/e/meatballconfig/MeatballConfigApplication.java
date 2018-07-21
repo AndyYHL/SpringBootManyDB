9080e1350684c2f8451e0d57299a237dffe6cae0
Andy <love90yhl@hotmail.com>
2018-07-21 15:44:29 +0800

    加入gatawey

--- meatball-config/src/main/java/com/qxst/e/meatballconfig/MeatballConfigApplication.java
+++ meatball-config/src/main/java/com/qxst/e/meatballconfig/MeatballConfigApplication.java
@@ -2,10 +2,12 @@ package com.qxst.e.meatballconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer  //配置中心服务
+@EnableDiscoveryClient //注册客户端
 public class MeatballConfigApplication {
 
     public static void main(String[] args) {
--- meatball-gateway/src/main/java/com/qxst/e/meatballgateway/MeatballgatewayApplication.java
+++ meatball-gateway/src/main/java/com/qxst/e/meatballgateway/MeatballgatewayApplication.java
@@ -2,10 +2,10 @@ package com.qxst.e.meatballgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient //注册客户端
 public class MeatballgatewayApplication {
 
     public static void main(String[] args) {
