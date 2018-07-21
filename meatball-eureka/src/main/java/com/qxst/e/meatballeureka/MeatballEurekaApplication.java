e51c6fcf84095ccd1219fcd5efaf58503095b5e8
Andy <love90yhl@hotmail.com>
2018-07-21 11:51:06 +0800

    加入批量插入

---
+++ meatball-eureka/src/main/java/com/qxst/e/meatballeureka/MeatballEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.qxst.e.meatballeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeatballEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeatballEurekaApplication.class, args);
+    }
+}
