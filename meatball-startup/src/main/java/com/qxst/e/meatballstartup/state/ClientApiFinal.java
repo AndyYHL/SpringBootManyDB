977f2f383ee3dfe923e6540b1fb0852dbc319a78
Andy <love90yhl@hotmail.com>
2018-07-21 09:30:49 +0800

    加入批量插入

--- meatball-startup/src/main/java/com/qxst/e/meatballstartup/state/ClientApiFinal.java
+++ meatball-startup/src/main/java/com/qxst/e/meatballstartup/state/ClientApiFinal.java
@@ -6,4 +6,8 @@ package com.qxst.e.meatballstartup.state;
 public class ClientApiFinal {
     public static final String version ="/api/v1/"; //接口版本
     public static final String file_upload = "file_upload/"; //文件上传目录
+
+    //众智联邦
+    public static final String zzlb = "http://47.96.26.250:84/";
+    public static final String goodsType = "rest/service/product/goodsType/list";
 }
