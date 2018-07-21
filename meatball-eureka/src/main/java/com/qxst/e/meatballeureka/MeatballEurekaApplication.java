d04ce02c6e76c2175e87b7440597936badbd2a1e
Andy <love90yhl@hotmail.com>
2018-07-21 11:58:36 +0800

    加入批量插入

--- meatball-eureka/src/main/java/com/qxst/e/meatballeureka/MeatballEurekaApplication.java
+++ meatball-eureka/src/main/java/com/qxst/e/meatballeureka/MeatballEurekaApplication.java
@@ -2,8 +2,10 @@ package com.qxst.e.meatballeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer  //@EnableEurekaServer注解启动服务注册中心
 public class MeatballEurekaApplication {
 
     public static void main(String[] args) {
